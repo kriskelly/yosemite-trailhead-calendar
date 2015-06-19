26a6457261e753ff44f8e84b9de26d49cf4fe042
Kristopher Kelly <kristopherbkelly@gmail.com>
2015-06-18 17:35:28 -0700

    Remove long timeout

--- test/yosemite/scraper_test.js
+++ test/yosemite/scraper_test.js
@@ -3,7 +3,6 @@ var scrapePdf = require('../../app/scraper/yosemite'),
 
 describe('scraper', function() {
   it('works', function(done) {
-    this.timeout(15000); // 15s
     scrapePdf().then(function(pages) {
       expect(pages).to.be.instanceOf(Array);
     }).then(done);
